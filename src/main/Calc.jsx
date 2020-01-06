39e8e14ab190cf2c739935a0b5e04b81ab656241
Arjios <antonio.uab.ios.fisica@gmail.com>
2020-01-06 15:09:53 -0300

    Incluido Background Calculadora

---
+++ src/main/Calc.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './Calc.css'
+
+export default class Calc extends Component {
+    render() {
+        return(
+            <div className="calc">
+
+            </div>
+        )
+    }
+}
