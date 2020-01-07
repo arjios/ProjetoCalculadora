19dbc4628798c9d5fb7b229d68405006674cefc1
Arjios <antonio.uab.ios.fisica@gmail.com>
2020-01-07 13:34:19 -0300

    Projeto atualizacao Calc

---
+++ src/Components/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import './Display.css'
+
+export default props => 
+    <div ClassName='display'>{props.value}</div>
+
--- src/main/Calc.jsx
+++ src/main/Calc.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react'
 import './Calc.css'
+import Botao from '../Components/Botao'
+import Display from '../Components/Display'
 
 export default class Calc extends Component {
     render() {
         return(
             <div className="calc">
-
+                <Botao label = 'AC' />
+                <Botao label = '+/-' />
+                <Botao label = '%' />
+                <Botao label = '/' />
+                <Botao label = '7' />
+                <Botao label = '8' />
+                <Botao label = '9' />
+                <Botao label = '*' />
+                <Botao label = '4' />
+                <Botao label = '5' />
+                <Botao label = '6' />
+                <Botao label = '-' />
+                <Botao label = '1' />
+                <Botao label = '2' />
+                <Botao label = '3' />
+                <Botao label = '+' />
+                <Botao label = '=' />
             </div>
         )
     }
